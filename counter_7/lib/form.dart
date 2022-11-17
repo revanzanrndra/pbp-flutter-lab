92f7a1a61d0091b21ce7a146909cba023bc6cbe5
TM Revanza <revanzanrndra@gmail.com>
2022-11-17 11:57:48 +0700

    Tugas 8

---
+++ counter_7/lib/form.dart
@@ -0,0 +1,213 @@
+import 'package:counter_7/main.dart';
+import 'package:counter_7/data.dart';
+import 'package:flutter/material.dart';
+
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key});
+
+  @override
+  State<MyFormPage> createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<MyFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _judul = "";
+  int _nominal = 0;
+  String jenisBudget = "pemasukan";
+  List<String> listJenisBudget = ['pemasukan', 'pengeluaran'];
+
+  bool isNumeric(String s) {
+    return num.tryParse(s) != null;
+  }
+
+  Card dataCard(String judul, int nominal, String jenisBudget) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  judul,
+                  style: const TextStyle(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, bottom: 15.0, top: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('$nominal'),
+                Text(jenisBudget),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form Budget'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text('counter_7'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Tambah Budget'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Data Budget'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyDataPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Contoh: Beli laptop",
+                          labelText: "Judul",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        onChanged: (String? value) {
+                          setState(() {
+                            _judul = value!;
+                          });
+                        },
+                        onSaved: (String? value) {
+                          setState(() {
+                            _judul = value!;
+                          });
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Judul tidak boleh kosong!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Contoh: 5000000",
+                          labelText: "Nominal",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        onChanged: (String? value) {
+                          if (isNumeric(value!)) {
+                            setState(() {
+                              _nominal = int.parse(value);
+                            });
+                          }
+                        },
+                        onSaved: (String? value) {
+                          if (isNumeric(value!)) {
+                            setState(() {
+                              _nominal = int.parse(value);
+                            });
+                          }
+                        },
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Nominal tidak boleh kosong!';
+                          }
+
+                          if (!isNumeric(value)) {
+                            return 'Nominal harus dalam bentuk angka!';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: const Text(
+                        'Pilih Jenis',
+                      ),
+                      trailing: DropdownButton(
+                        value: jenisBudget,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: listJenisBudget.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            jenisBudget = newValue!;
+                          });
+                        },
+                      ),
+                    ),
+                  ]),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    listData.add(dataCard(_judul, _nominal, jenisBudget));
+                    _formKey.currentState!.reset();
+                  }
+                },
+                child: const Text(
+                  "Simpan",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- counter_7/lib/main.dart
+++ counter_7/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:counter_7/form.dart';
+import 'package:counter_7/data.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,13 +26,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Program Counter'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,7 +43,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title = "Program Counter";
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -87,6 +89,40 @@ class _MyHomePageState extends State<MyHomePage> {
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text('counter_7'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyHomePage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Tambah Budget'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyFormPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('Data Budget'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MyDataPage()),
+                  );
+                },
+              ),
+              
+            ],
+          ),
         ),
         body: Center(
           // Center is a layout widget. It takes a single child and positions it
